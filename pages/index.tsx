ba80a39666925572c463e66c2fc2883b10c6eebc
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-05-01 04:55:37 +0545

    whos-watching-profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from '@/hooks/userCurrentUser';
 import { NextPageContext } from 'next';
 import { getSession, signOut } from 'next-auth/react';
 
@@ -19,9 +20,11 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <>
       <h1 className="text-rose-500 font-semibold">Netflix Clone</h1>
+      <p className="text-2xl text-white">Logged in as : {user?.name}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         Logout!
       </button>
