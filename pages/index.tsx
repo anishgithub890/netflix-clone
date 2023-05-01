a3fa04132f2a3caa40a0e7abc193824436f70689
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-05-01 17:37:59 +0545

    navbar-component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import useCurrentUser from '@/hooks/userCurrentUser';
+import Navbar from '@/components/Navbar';
 import { NextPageContext } from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,14 +20,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
   return (
     <>
-      <h1 className="text-rose-500 font-semibold">Netflix Clone</h1>
-      <p className="text-2xl text-white">Logged in as : {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <Navbar />
     </>
   );
 }
